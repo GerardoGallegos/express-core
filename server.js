64c5b448e55f8bad8254c842f5b0574ff2881489
Gerardo Gallegos <puxerop@gmail.com>
2019-02-12 17:46:15 -0600

    Leccion # 10 - Usando motores de plantillas en Express.js

--- server.js
+++ server.js
@@ -1,42 +1,38 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const consolidate = require('consolidate')
 
-const checkAuth = (config) => {
-  const message = config.message || 'Debes iniciar Sesion 😅'
+app.set('views', './views')
+app.engine('hbs', consolidate.handlebars)
+app.set('view engine', 'hbs')
 
-  return (req, res, next) => {
-    const isAuth = false
-  
-    if (isAuth) {
-      next()
-    } else {
-      res.send(message)
-    }
-  }
-}
+app.get('/perfil', (req, res) => {
+  res.render('perfil', {
+    name: 'Gerardo Gallegos',
+    favorites: [
+       'React',
+       'Vue',
+       'Angular',
+       'RXjs',
+       'Node.js',
+       'Typescript'
+    ],
 
-app.get('/publica', (req, res) => {
+    courses: [
+      { title: 'React Avanzado' },
+      { title: 'React Router' },
+      { title: 'Node.js Fundamentos' },
+      { title: 'Angular Fundamentos' }
+    ]
+  })
+})
+
+app.get('/', (req, res) => {
   res.send(`
-    <h1>Ruta Publica</h1>
+    <h1>Motores de Plantillas ⚡️</h1>
+    <h2>Template Engines</h2>
   `)
-})
-
-const isAuthCustom = checkAuth({
-  message: 'Please Login 💂‍'
-})
-
-app.get('/privada', isAuthCustom, (req, res) => {
-  res.send(`
-    <h1>Ruta Privada 😎</h1>
-  `)
-})
-
-
-app.get('*', (req, res) => {
-  res
-    .status(404)
-    .send(`<h1>Woops pagina no existe!</h1>`)
 })
 
 app.listen(PORT, () => {
